bd2c2ed411ec5a083a410b4e81b923dff5f8871e
Ulisse Mini <ulisse.mini@gmail.com>
2021-01-17 13:48:44 -0500

    Add time data to search_stats

--- src/bin/search_stats.rs
+++ src/bin/search_stats.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use yobmef::chess::Board;
 use yobmef::movegen::gen_moves_once;
 use yobmef::search::Searcher;
@@ -10,7 +11,16 @@ fn main() {
     for depth in 3..7 {
         eprintln!("depth: {}", depth);
         let mut searcher = Searcher::new();
-        let sr = searcher.search(&board, depth);
-        eprintln!("nodes {} pruned {}\n", searcher.nodes, searcher.pruned);
+
+        let start = Instant::now();
+        searcher.search(&board, depth);
+        let took = Instant::now() - start;
+
+        eprintln!(
+            "nodes {} pruned {} took {}ms\n",
+            searcher.nodes,
+            searcher.pruned,
+            took.as_millis()
+        );
     }
 }
