aad62f1d8e89549ae03c9ef7d8490ad78a5fcc70
Ulisse Mini <ulisse.mini@gmail.com>
2021-01-17 09:10:29 -0500

    Default position to startpos

--- src/engine.rs
+++ src/engine.rs
@@ -1,19 +1,19 @@
 use crate::chess::Board;
-use crate::uci;
 use crate::movegen::{get_legal_moves, perft};
 use crate::search::Searcher;
+use crate::uci;
 use crate::uci::EngineMessage;
 use std::io;
 
 pub struct Engine {
-    position: Option<Board>,
+    position: Board,
     searcher: Searcher,
 }
 
 impl Engine {
     pub fn new() -> Engine {
         Engine {
-            position: None,
+            position: Board::from_start_pos(),
             searcher: Searcher::new(),
         }
     }
@@ -38,10 +38,10 @@ impl Engine {
     }
 
     fn perft(&self, depth: u16) {
-        let board = &Board::from_start_pos();
+        let board = &self.position;
 
         let mut nodes = 0;
-        for mv in get_legal_moves(board) {
+        for mv in get_legal_moves(&board) {
             let n = perft(&board.make_move(&mv), depth - 1);
             eprintln!("{}: {}", mv, n);
             nodes += n;
@@ -57,17 +57,11 @@ impl Engine {
             return;
         }
 
-        match &self.position {
-            None => {
-                eprintln!("No position specified");
-            }
+        let search_result = self
+            .searcher
+            .search(&self.position, opts.depth.unwrap_or(4));
 
-            Some(board) => {
-                let search_result = self.searcher.search(&board, opts.depth.unwrap_or(4));
-
-                println!("bestmove {}", search_result.mv.unwrap());
-            }
-        }
+        println!("bestmove {}", search_result.mv.unwrap());
     }
 
     fn handle(&mut self, msg: uci::EngineMessage) {
@@ -86,7 +80,7 @@ impl Engine {
                     board.make_move_mut(&movement);
                 }
                 eprintln!("current position:\n{}", board);
-                self.position = Some(board);
+                self.position = board;
             }
 
             EngineMessage::Go(opts) => self.go(opts),
@@ -104,7 +98,7 @@ mod tests {
     fn test_position() {
         let mut engine = Engine::new();
         engine.handle(uci::parse("position startpos").unwrap());
-        assert_eq!(engine.position, Some(Board::from_start_pos()));
+        assert_eq!(engine.position, Board::from_start_pos());
 
         engine.handle(uci::parse("position startpos moves e2e4 e7e5").unwrap());
         // NOTE: we assert for en-passant e6, that was failing earlier because lichess
@@ -112,10 +106,11 @@ mod tests {
         assert_eq!(
             engine.position,
             Board::from_fen("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2")
+                .unwrap()
         );
 
         let fen = "K1k5/8/8/8/8/8/8/8 w - - 0 1";
         engine.handle(uci::parse(&format!("position fen {}", fen)).unwrap());
-        assert_eq!(engine.position, Board::from_fen(fen));
+        assert_eq!(engine.position, Board::from_fen(fen).unwrap());
     }
 }
